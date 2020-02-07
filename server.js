27edf86366e1875c3f1abd3fa7b9e1e2bab3852b
FelipePRodrigues <felipeprodrigues@outlook.com.br>
2020-02-07 13:10:23 -0300

    Add routes; Add project folders

--- server.js
+++ server.js
@@ -1,33 +1,12 @@
+const PORT = process.env.PORT || 7000;
 const express = require('express');
-const people = require('./people.json');
 const app = express();
 
+app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'pug');
 
-app.use(express.static(__dirname + '/public'));
-
-app.get('/', (req, res) => {
-    res.render('index', {  
-        title: 'Snippets'
-        ,people: people.profiles
-    });
-});
-
-app.get('/profile', (req, res) => {
-    const person = people.profiles.find(p => p.id === req.query.id);
-    res.render('profile', {
-        title: `About ${person.firstname} ${person.lastname}`,
-        person,
-    });
-});
-
-app.get('/login', (req, res) => {
-    res.render('login', {
-        title: 'Login'
-    });
-});
-
-const PORT = process.env.PORT || 7000;
+app.use('/', require('./routes/index'));
+app.use('/login', require('./routes/login'));
 
 const server = app.listen(PORT, () => {
     console.log(`Express running → PORT ${server.address().port}`);
