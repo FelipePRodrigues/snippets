ac9cadf25f774b8478d6a86196c738498afa1864
FelipePRodrigues <felipeprodrigues@outlook.com.br>
2020-02-19 15:45:21 -0300

    Add pg module; Add a test query

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -8,5 +8,29 @@ exports.createPost = function(req, res, next){
     let email = req.body.email;
     let password = req.body.password;      
     
+    const { Client } = require('pg');
+    
+    const client = new Client({
+        user: 'odymyorzclkmua',
+        host: 'ec2-52-45-75-24.compute-1.amazonaws.com',
+        database: 'd2gms89mo99to7',
+        password: '57b10c2232ca733eddb1935e4fee6297536f3452e8c90436a779b9ad3bdf4b1b',
+        port: 5432,
+        ssl: {
+            rejectUnauthorized: false
+        }        
+    });
+    
+    client.connect();
+    
+    client.query('SELECT NOW() as now', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            console.log(JSON.stringify(row));
+        }
+        client.end();
+    });
+    
+    
     res.send(email + ' ' + password);
 }
